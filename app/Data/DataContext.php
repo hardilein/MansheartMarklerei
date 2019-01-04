87be9503ea175e6990d123ba73fc966cd14ef98a
Vitalij Befus <vitalij@befus.com>
2019-01-04 04:06:15 +0100

    * Locale Entwicklungsumgebung abfragen, DB Host und DB Port setzen

--- app/Data/DataContext.php
+++ app/Data/DataContext.php
@@ -5,10 +5,8 @@ class Database
     //TODO: webconfig
 
     private static $db = 'mansheart';
-    // private static $host = 'localhost';
-    private static $host = '127.0.0.1';
-    // private static $port = 3306; // Default
-    private static $port = 32780; // Vitalij Docker
+    private static $host = 'localhost';
+    private static $port = 3306;
     private static $user = 'root';
     private static $password = '';
 
@@ -21,6 +19,13 @@ class Database
 
     public static function connect()
     {
+
+        // temporär für die locale Entwicklungsumgebung, to be deleted
+        if($_SERVER['SERVER_PORT'] == '8080') {
+            self::$host = '127.0.0.1';
+            self::$port = 32780;
+        }
+
         // Verhindern das ein Client mehrere Verbindungen aufbaut
         if (null == self::$cont) {
             try
